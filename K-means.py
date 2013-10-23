548ece544ec18868a69d52ec70a6e8516686095e
ChiaLunLiu <h3355626@msn.com>
2013-10-23 23:01:03 +0800

    code refactor

--- K-means.py
+++ K-means.py
@@ -9,9 +9,11 @@ class Point:
         self.y = y
         self.group = -1
         self.objectid = -1
+        self.color="0x00"
 class Kmeans:
     def __init__(self):
-        self.color = [] #'''color used by K points'''
+        self.height = 600 # GUI height
+        self.width = 800  # GUI width
         self.k = 0      #'''number of K'''
         self.p = []     #'''sample points'''
         self.gp = []    #'''points of K'''
@@ -20,7 +22,8 @@ class Kmeans:
         self.initGUI()
     def initGUI(self):
         self.top = tkinter.Tk()
-        self.C = tkinter.Canvas(self.top, bg="white", height=400, width=600)
+        self.top.title("K-means")
+        self.C = tkinter.Canvas(self.top, bg="white", height=self.height, width=self.width)
         self.var = tkinter.StringVar()
         self.var1 = tkinter.StringVar()
         self.topFrame = tkinter.Frame(self.top)
@@ -39,15 +42,14 @@ class Kmeans:
         self.button_run = tkinter.Button(self.topFrame, text ="run", command = self.run)
         self.button_run.pack(side=tkinter.LEFT)
         self.label = tkinter.Label(self.top,textvariable=self.var)
-
         self.var.set(("run :%d" % (self.cnt)))
-        self.label.pack()
         self.topFrame.pack()
         self.C.pack()
+        self.label.pack()
     def run(self):
         self.update()
         if( self.finished == 0):
-            self.top.after(2000,self.run)
+            self.top.after(1000,self.run)
     def init2(self):
         k = int(self.entryK.get())
         n = int(self.entryNum.get())
@@ -58,32 +60,29 @@ class Kmeans:
         self.finished = 0
         self.num = n
         self.k = k
-        self.color=[]
         self.p=[]
         self.gp=[]
         for i in range(0,k):
             colorString = '#%02x%02x%02x' %(random.randint(0,256),random.randint(0,256),random.randint(0,256))
-            self.color.append(colorString)
             t = Point()
-            t.x = random.randint(5,590)
-            t.y = random.randint(5,390)
+            t.color = colorString
+            t.x = random.randint(5,self.width-5)
+            t.y = random.randint(5,self.height-5)
             self.gp.append(t)
         for i in range(0,n):
             t = Point()
-            t.x = random.randint(5,590)
-            t.y = random.randint(5,390)
+            t.color ="green"
+            t.x = random.randint(5,self.width-5)
+            t.y = random.randint(5,self.height-5)
             self.p.append(t)
     def draw(self):
-       # idx = self.C.create_oval(90,90,100,100, fill="blue")
-       # idx = self.C.create_oval(150,150,160,160, fill="blue")
-       # idx = self.C.create_oval(0,0,10,10, fill="blue")
         for i in range(0,self.num):
             pt = self.p[i]
-            idx = self.C.create_oval(pt.x-5,pt.y-5,pt.x+5,pt.y+5, fill="green")
+            idx = self.C.create_oval(pt.x-5,pt.y-5,pt.x+5,pt.y+5, fill= pt.color)
             pt.objectid = idx;
         for i in range(0,self.k):
             pt = self.gp[i]
-            idx = self.C.create_oval(pt.x-5,pt.y-5,pt.x+5,pt.y+5, fill=self.color[i])
+            idx = self.C.create_oval(pt.x-5,pt.y-5,pt.x+5,pt.y+5, fill= pt.color)
             pt.objectid = idx;
     def update(self):
         if( self.finished == 1):
@@ -104,7 +103,7 @@ class Kmeans:
                     idx = j
             self.p[i].group = idx
             count[idx] = count[idx]+1;
-            self.C.itemconfig(self.p[i].objectid,fill=self.color[idx])
+            self.C.itemconfig(self.p[i].objectid,fill=self.gp[idx].color)
             new_gp[ idx ].x += self.p[i].x
             new_gp[ idx ].y += self.p[i].y
 
@@ -119,28 +118,17 @@ class Kmeans:
 
         if changed == 0:
             self.finished = 1
-            self.var.set("done")
-            return True
-        ''' update gp'''
+            self.var.set("(run : %d)" %(self.cnt))
+            return
+ #       ''' update gp'''
         for i in range(0,self.k):
            self.gp[i] = new_gp[i]
         for i in range(0,self.k):
             self.C.delete(self.gp[i].objectid)
             pt = self.gp[i]
-            idx = self.C.create_oval(pt.x-5,pt.y-5,pt.x+5,pt.y+5, fill=self.color[i])
+            idx = self.C.create_oval(pt.x-5,pt.y-5,pt.x+5,pt.y+5, fill= pt.color)
             self.gp[i].objectid = idx
-        return False
-
-    def print12(self):
-        return self.num
-
+        return
 k = Kmeans()
-#k.init(5,300)
-
-#k.draw()
-#k.top.after(1000,k.update)
-
 k.top.mainloop()
 
-
-print("done")
